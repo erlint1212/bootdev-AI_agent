a51f9fbe080c396e730c89d08ed74fc7b2b90830
erlint1212 <e.t.nordtvedt@gmail.com>
2025-09-29 00:35:12 +0200

    1.4 input prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -6,16 +7,25 @@ from google import genai
 def main():
     print("Hello from ai-agent!")
 
+    match len(sys.argv):
+        case 2:
+            if not isinstance(sys.argv[1], str):
+                raise Exception(f"Argument must be a string, input was: {type(sys.argv[1])}")
+        case 1:
+            raise Exception("No prompt given")
+        case _:
+            raise Exception("Too mangy arguments given")
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     client = genai.Client(api_key=api_key)
 
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    prompt = sys.argv[1] #"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     models = ["gemini-2.0-flash-001", "gemini-2.5-flash"]
 
     response = client.models.generate_content(
-        model=models[1], contents=prompt
+        model=models[0], contents=prompt
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
