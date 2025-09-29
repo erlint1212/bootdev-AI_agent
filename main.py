2f41915f5417569629ab79eaa5dbc98663c985b8
erlint1212 <e.t.nordtvedt@gmail.com>
2025-09-29 23:47:53 +0200

    1.5 setup for conversation, history

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 
 def main():
@@ -21,11 +23,16 @@ def main():
 
     client = genai.Client(api_key=api_key)
 
-    prompt = sys.argv[1] #"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
     models = ["gemini-2.0-flash-001", "gemini-2.5-flash"]
 
+    user_prompt = sys.argv[1] #"Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
     response = client.models.generate_content(
-        model=models[0], contents=prompt
+        model=models[0], 
+        contents=messages
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
