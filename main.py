15fb11af94ed59167dd76d4cf750a5c195a6df41
erlint1212 <e.t.nordtvedt@gmail.com>
2025-10-04 20:39:29 +0200

    3.2 Function Declaration, get LLM to call premade functions

--- main.py
+++ main.py
@@ -5,25 +5,15 @@ from google import genai
 from google.genai import types
 import argparse
 import config
+from functions.get_files_info import schema_get_files_info
 
-def verbose_print(user_prompt, response) -> None:
+def verbose_print(user_prompt : str, response : types.GenerateContentResponse) -> None:
     print(f"User prompt: {user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 def main() -> None:
     print("Hello from ai-agent!")
-
-    """
-    match len(sys.argv):
-        case 2:
-            if not isinstance(sys.argv[1], str):
-                raise Exception(f"Argument must be a string, input was: {type(sys.argv[1])}")
-        case 1:
-            raise Exception("No prompt given")
-        case _:
-            raise Exception("Too mangy arguments given")
-    """
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
@@ -49,12 +39,29 @@ def main() -> None:
         types.Content(role="user", parts=[types.Part(text=args.user_prompt)]),
     ]
 
+    available_functions = types.Tool(
+        function_declarations=[
+            schema_get_files_info,
+        ]
+    )
+
     response = client.models.generate_content(
         model=models[0], 
         contents=messages,
-        config=types.GenerateContentConfig(system_instruction=config.SYSTEM_PROMPT)
+        config=types.GenerateContentConfig(
+            tools=[available_functions], 
+            system_instruction=config.SYSTEM_PROMPT
+            )
     )
+
+    functions_called = response.function_calls
+
     print(response.text)
+
+    if len(functions_called) != 0:
+        for function_call_part in functions_called: 
+            print(f"Calling function: {function_call_part.name}({function_call_part.args})")
+
     if args.verbose:
         verbose_print(args.user_prompt, response)
 
